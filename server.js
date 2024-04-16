c853558d7ab75bebc2901be6555da059532eb21e
prasanthshan369 <90073917+prasanthshan369@users.noreply.github.com>
2024-04-16 15:52:36 +0530

    server.js

--- server.js
+++ server.js
@@ -1,32 +1,4 @@
-// const express = require("express");
-// const app = express();
-// require('dotenv').config()
-
-// app.get('/',(req,res)=>{
-//   const {code,state} =req.query
-//   console.log(code,state);
-//   res.redirect(`https://prego-server.onrender.com/catchOauthToken?code=${code}&state=${state}`)
-  
-// })
-
-// app.get('/get',(rea,res)=>{
-//     console.log(process.env.PORT);
-// })
-// app.get("/catchOauthToken", (req, res) => {
-//   const authorizationCode = req.query.code;
-//   if (authorizationCode) {
-//     console.log("Received Authorization Code:", authorizationCode);
-//     // res.json(authorizationCode)
-//     res.redirect('https://pregoclient.netlify.app/?token='+authorizationCode);
-//   } else {
-//     res.send("Error: No authorization code provided.");
-//   }
-// });
-// app.listen(process.env.PORT||3000, () => {
-//   console.log("Server is running and waiting for the authorization code.");
-// });
- //----------------------local connection uri----------------
- const express = require("express");
+const express = require("express");
 const app = express();
 require('dotenv').config()
 
@@ -45,7 +17,7 @@ app.get("/catchOauthToken", (req, res) => {
   if (authorizationCode) {
     console.log("Received Authorization Code:", authorizationCode);
     // res.json(authorizationCode)
-    res.redirect('http://localhost:3000?token='+authorizationCode);
+    res.redirect('https://pregoclient.netlify.app/?token='+authorizationCode);
   } else {
     res.send("Error: No authorization code provided.");
   }
@@ -53,3 +25,31 @@ app.get("/catchOauthToken", (req, res) => {
 app.listen(process.env.PORT||3000, () => {
   console.log("Server is running and waiting for the authorization code.");
 });
+ //----------------------local connection uri----------------
+//  const express = require("express");
+// const app = express();
+// require('dotenv').config()
+
+// app.get('/',(req,res)=>{
+//   const {code,state} =req.query
+//   console.log(code,state);
+//   res.redirect(`https://prego-server.onrender.com/catchOauthToken?code=${code}&state=${state}`)
+  
+// })
+
+// app.get('/get',(rea,res)=>{
+//     console.log(process.env.PORT);
+// })
+// app.get("/catchOauthToken", (req, res) => {
+//   const authorizationCode = req.query.code;
+//   if (authorizationCode) {
+//     console.log("Received Authorization Code:", authorizationCode);
+//     // res.json(authorizationCode)
+//     res.redirect('http://localhost:3000?token='+authorizationCode);
+//   } else {
+//     res.send("Error: No authorization code provided.");
+//   }
+// });
+// app.listen(process.env.PORT||3000, () => {
+//   console.log("Server is running and waiting for the authorization code.");
+// });
